290cb6195608bb307d01215b536a41eeaba45552
NdekoCode <arickbulakali@gmail.com>
2023-03-06 21:05:32 +0200

    feat: add data binding add attribute binding

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss'],
+})
+export class FaceSnapComponent implements OnInit {
+  // On met le point d'exclamation pour dire que c'est une variable qui ne sera jamais null et sera toujours de type string
+  title!: string;
+  description!: string;
+  imageURL!: string;
+  createdDate!: Date;
+  /** Pour les likes sur une photos */
+  snaps!: number;
+
+  ngOnInit() {
+    this.title = 'Lorem Ipsum is simply';
+    this.description =
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ';
+    this.imageURL =
+      'https://images.unsplash.com/photo-1531482615713-2afd69097998?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmxhY2slMjB3b21lbiUyMHdvcmt8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60';
+    this.createdDate = new Date();
+    this.snaps = 5;
+  }
+}
