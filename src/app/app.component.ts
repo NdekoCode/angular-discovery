2b4dcfd66dc601d2ac4e8178c8356443dfa47478
NdekoCode <arickbulakali@gmail.com>
2023-03-07 21:07:58 +0200

    feat: init ngFor directive for mapping array data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,44 +8,45 @@ import { FaceSnap } from './models/face-snap.model';
 })
 export class AppComponent implements OnInit {
   snap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
   secondSnap!: FaceSnap;
   thirdSnap!: FaceSnap;
   typeSnap!: string;
 
   ngOnInit(): void {
     this.typeSnap = 'Photos';
-    this.snap = {
-      title: 'Lorem Ipsum is simply',
+    this.faceSnaps = [
+      {
+        title: 'Lorem Ipsum is simply',
 
-      description:
-        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+        description:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
 
-      imageURL:
-        'https://images.unsplash.com/photo-1531482615713-2afd69097998?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmxhY2slMjB3b21lbiUyMHdvcmt8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-      createdDate: new Date(),
-      snaps: 7,
-      location: 'Paris',
-    };
-
-    this.secondSnap = {
-      title: 'Lorem Ipsum is simply',
-      description:
-        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
-      imageURL:
-        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      createdDate: new Date(),
-      snaps: 5,
-      location: 'Congo RDC',
-    };
-
-    this.thirdSnap = {
-      title: 'Lorem Ipsum is simply',
-      description:
-        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
-      imageURL:
-        'https://images.unsplash.com/photo-1571442463800-1337d7af9d2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsYWNrJTIwd29tZW4lMjB3b3JrfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-      createdDate: new Date(),
-      snaps: 0,
-    };
+        imageURL:
+          'https://images.unsplash.com/photo-1531482615713-2afd69097998?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmxhY2slMjB3b21lbiUyMHdvcmt8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+        createdDate: new Date(),
+        snaps: 7,
+        location: 'Paris',
+      },
+      {
+        title: 'Lorem Ipsum is simply',
+        description:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+        imageURL:
+          'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snaps: 5,
+        location: 'Congo RDC',
+      },
+      {
+        title: 'Lorem Ipsum is simply',
+        description:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+        imageURL:
+          'https://images.unsplash.com/photo-1571442463800-1337d7af9d2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsYWNrJTIwd29tZW4lMjB3b3JrfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        createdDate: new Date(),
+        snaps: 0,
+      },
+    ];
   }
 }
