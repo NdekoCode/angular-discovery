bebb8b2291edd515df5cf6404c398dade5dc3e93
NdekoCode <arickbulakali@gmail.com>
2023-03-07 11:10:32 +0200

    feat: add toggle like snap shoot

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -11,8 +11,10 @@ export class FaceSnapComponent implements OnInit {
   description!: string;
   imageURL!: string;
   createdDate!: Date;
+  isLiked!: boolean;
   /** Pour les likes sur une photos */
   snaps!: number;
+  buttonText!: string;
 
   ngOnInit() {
     this.title = 'Lorem Ipsum is simply';
@@ -22,8 +24,16 @@ export class FaceSnapComponent implements OnInit {
       'https://images.unsplash.com/photo-1531482615713-2afd69097998?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmxhY2slMjB3b21lbiUyMHdvcmt8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60';
     this.createdDate = new Date();
     this.snaps = 5;
+    this.isLiked = false;
+    this.buttonText = "J'aime";
   }
-  onAddSnap() {
-    this.snaps++;
+  onSnap() {
+    if (this.isLiked) {
+      this.snaps--;
+      this.isLiked = false;
+    } else {
+      this.snaps++;
+      this.isLiked = true;
+    }
   }
 }
