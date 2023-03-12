1427af3f80eb8df516b8b6c5dbe4ed875c701d2e
NdekoCode <arickbulakali@gmail.com>
2023-03-12 15:32:21 +0200

    [data-binding] add attribute binding add string interpolation

--- src/app/blog/pages/article/article.component.ts
+++ src/app/blog/pages/article/article.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Post } from 'src/utils/types';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.scss']
+  styleUrls: ['./article.component.scss'],
 })
 export class ArticleComponent {
-
+  @Input() post!: Post;
 }
