88f8d892175a4c69886821092f4002976d32f689
NdekoCode <arickbulakali@gmail.com>
2023-03-26 08:20:53 +0200

    feat: [rxjs] use operator and subsription

--- src/app/blog/article-list/article-list.component.ts
+++ src/app/blog/article-list/article-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Observable, Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 import { Product } from 'src/utils/types';
 import { ProductService } from '../../services/product.service';
 
@@ -8,14 +10,30 @@ import { ProductService } from '../../services/product.service';
   styleUrls: ['./article-list.component.scss'],
   providers: [ProductService],
 })
-export class ArticleListComponent implements OnInit {
+export class ArticleListComponent implements OnInit, OnDestroy {
+  secondes: number | string = 0;
+  compterSubscription!: Subscription;
   products!: Product[];
   message!: string;
   constructor(private _productsService: ProductService) {}
   ngOnInit() {
     this.products = this._productsService.getAllProducts();
+    const compter: Observable<number | string> = interval(1000).pipe(
+      filter((v) => v % 2 === 0),
+      map((v) =>
+        v % 2 === 0 ? `Le nombre ${v} est paire` : `Le nombre ${v} est impaire`
+      )
+    );
+    this.compterSubscription = compter.subscribe({
+      next: (v) => (this.secondes = v),
+      error: (e) => console.error(e),
+      complete: () => console.log('Complete'),
+    });
   }
   onGetInfo(arg: string) {
     this.message = arg;
+  }
+  ngOnDestroy() {
+    this.compterSubscription.unsubscribe();
   }
 }
